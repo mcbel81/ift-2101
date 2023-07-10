e01ed3b155fc1aa5892c294408342c226009ff09
Marie-Christine Bélanger <mcbel81@ulaval.ca>
2023-07-10 15:54:55 -0400

    début des fichiers

---
+++ TP3-equipe18/src/villes.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ma page statique</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      background-color: #f2f2f2;
+      color: #333;
+      margin: 0;
+      padding: 20px;
+      text-align: center;
+    }
+
+    h1 {
+      font-size: 24px;
+      margin-top: 0;
+    }
+
+    .contact-info {
+      max-width: 400px;
+      margin: 0 auto;
+      background-color: #fff;
+      padding: 20px;
+      border-radius: 5px;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    }
+
+    .contact-info p {
+      margin: 10px 0;
+    }
+
+    .contact-info strong {
+      font-weight: bold;
+    }
+  </style>
+</head>
+<body>
+  <h1>Équipe 18</h1>
+    
+
+</body>
+</html>
